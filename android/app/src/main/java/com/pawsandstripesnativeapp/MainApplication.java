97c3adea2924c1a02f5e7566c7113500b53d843a
robert-spidle <robert.spidle@ruralsourcing.com>
2017-07-18 09:54:50 -0600

    Commented out packages that were stopping the app from building

--- android/app/src/main/java/com/pawsandstripesnativeapp/MainApplication.java
+++ android/app/src/main/java/com/pawsandstripesnativeapp/MainApplication.java
@@ -4,9 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 
-import com.wheelpicker.WheelPickerPackage;
+//import com.wheelpicker.WheelPickerPackage;
 //import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
-import com.wheelpicker.WheelPickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,10 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
 
-          new MainReactPackage(),
-            new WheelPickerPackage(),
+          new MainReactPackage()
+            //new WheelPickerPackage()
             //new RCTSplashScreenPackage(),
-            new WheelPickerPackage()
       );
     }
   };
